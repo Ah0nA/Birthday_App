92900f2faa406ea2c1aeb7dbb3e8e2ca74961df8
Amanda <amanda.ahona@student.moringaschool.com>
2021-09-07 17:21:50 +0300

    modified

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const form = document.getElementById("akanNames");
 const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku"," Yaw"," Kofi"," Kwame"];
 const femaleNames =["Akosua","Adwoa","Abenaa","Akua"," Yaa"," Afua","Ama"];
 
-function(){
+function (akanNames){
 const CC = centuryFromYear(fd.get("year"));
 const YY = parseInt(fd.get("year"));
 const MM = fd.get("month");
 const DD = fd.get("day");
 const day = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
-console.log(date);
+console.log(akanName);
 }
 if(fd.get("gender") == "male"){
 alertText.innerHTML = `YOUR AKAN NAME IS ${maleNames[day]}`;
