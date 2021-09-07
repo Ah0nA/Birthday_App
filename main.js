854c9b36a515749328bfd91b53ebf598a71fe304
Amanda <amanda.ahona@student.moringaschool.com>
2021-09-07 14:12:28 +0300

    modified

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const form = document.querySelector("akanNames")
-const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku"," Yaw"," Kofi"," Kwame"]
-const femaleNames =["Akosua","Adwoa","Abenaa","Akua"," Yaa"," Afua","Ama"]
+const form = document.querySelector("akanNames");
+const alertMe = document.querySelector("#alert");
+
+const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku"," Yaw"," Kofi"," Kwame"];
+const femaleNames =["Akosua","Adwoa","Abenaa","Akua"," Yaa"," Afua","Ama"];
 
 
-const CC = centuryFromYear;
-const YY =
-const MM =
-const DD =
+const CC = centuryFromYear(fd.get("year"));
+const YY = parseInt(fd.get("year"));
+const MM = fd.get("month");
+const DD = fd.get("day");
 const day = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+
+if(fd.get("gender") == "male"){
+alertText.innerHTML = `YOUR AKAN NAME IS.. ${maleNames[day]}`;
+
+}else{
+alertText.innerHTML =  `YOUR AKAN NAME IS.. ${femaleNames[day]}`;
+}
 
 
 
