7855422207a33a1474758265a30f9a3ff113e5e7
Amanda <amanda.ahona@student.moringaschool.com>
2021-09-07 13:45:47 +0300

    modified

--- main.js
+++ main.js
@@ -1,70 +1,24 @@
-let names = [
-  {
-      gender:'Male',
-      dayOfBirt: 'Sunday',
-      name:'Kwasi'
-  }, {
-      gender:'Male',
-      dayOfBirth: 'Monday',
-      name:'Kwado'
-  },
-  {
-      gender:'Male',
-      dayOfBirth: 'Tuesday',
-      name:'Kwabena'
-  },
-  {
-      gender:'Male',
-      dayOfBirth: 'Wednesday',
-      name:'Kwaku'
-  },
-  {
-      gender:'Male',
-      dayOfBirth: 'Thursday',
-      name:'Yaw'
-  },
-  {
-      gender:'Male',
-      dayOfBirth: 'Friday',
-      name:'Kofi'
-  },
-  {
-      gender:'Male',
-      dayOfBirth: 'Saturday',
-      name:'Kwame'
-  },
+const form = document.querySelector("akanNames")
+const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku"," Yaw"," Kofi"," Kwame"]
+const femaleNames =["Akosua","Adwoa","Abenaa","Akua"," Yaa"," Afua","Ama"]
 
 
-  {
-      gender:'Female',
-      dayOfBirth: 'Sunday',
-      name:'Akosua'
-  }, {
-      gender:'Female',
-      dayOfBirth: 'Monday',
-      name:'Adwoa'
-  },
-  {
-      gender:'Female',
-      dayOfBirth: 'Tuesday',
-      name:'Abenaa'
-  },
-  {
-      gender:'Female',
-      dayOfBirth: 'Wednesday',
-      name:'Akua'
-  },
-  {
-      gender:'Female',
-      dayOfBirth: 'Thursday',
-      name:'Yaa'
-  },{
-      gender:'Female',
-      dayOfBirth: 'Friday',
-      name:'Afua'
-  },{
-      gender:'Female',
-      dayOfBirth: 'Saturday',
-      name:'Ama'
-  }
-];
+const CC = centuryFromYear;
+const YY =
+const MM =
+const DD =
+const day = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+
+
+
+function centuryFromYear(year) {
+  if(typeof year == 'string')
+    if(year.toString().slice(-2) == '00')
+      return year.toString().slice(0,2);
+    else
+      return (Math.floor(+year/100) +1).toString();
+  else if(typeof year == 'number')
+    return Math.floor((year-1)/100) + 1;
+  else
+    return undefined;
+} 
